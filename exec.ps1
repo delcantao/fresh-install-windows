a3d277e29081e15d53c8f122c07bfe4991ed839d
Fernando del Cantão <accounting@delcantao.dev>
2022-07-20 21:35:12 +0200

    Create exec.ps1

---
+++ exec.ps1
@@ -0,0 +1,37 @@
+
+# Tools & Languages 
+choco install vcredist140 -y
+choco install fiddler -y
+choco install postman -y
+choco install insomnia-rest-api-client -y
+choco install microsoft-windows-terminal -y
+choco install mremoteng -y
+choco install office365proplus -y
+choco install docker-desktop -y
+choco install winrar -y
+choco install dotnet-windowshosting -y
+choco install nodejs -y
+choco install yarn -yarn
+choco install python2 -y
+choco install git -y
+
+# Editors
+choco install vscode -y
+choco install visualstudio-installer
+choco install visualstudio2022community -y
+choco install jetbrains-rider -y
+choco install resharper -y
+choco install sublimetext3 -y
+
+# Browsers
+choco install googlechrome -y
+choco install firefox-dev --pre -y
+
+# Databases
+choco install azure-data-studio -y
+choco install sql-server-management-studio
+choco install sql-server-2019 -y
+
+# Games
+choco install steamcmd -y
+choco install leagueoflegends -y
